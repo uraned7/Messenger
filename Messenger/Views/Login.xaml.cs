edc9e8f8a853a541feffd5eef49adf7467c2b334
Ivan Zakharov <uran-75@mail.ru>
2023-05-04 17:13:51 +0700

    V 0.1

---
+++ Messenger/Views/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Messenger.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        //Переход на страницу регистрации
+        private void RegBut_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Registration());
+        }
+
+        //Переход в сам мессенджер
+        private void AuthBut_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Messenger());
+        }
+    }
+}
